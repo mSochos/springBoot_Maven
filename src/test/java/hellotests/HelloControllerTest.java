f5218b2563e275d7a438ed9ca56c6a6451ba354d
mso139 <mso139@maersk.com>
2017-10-19 09:38:11 +0200

    failing unit test added

---
+++ src/test/java/hellotests/HelloControllerTest.java
@@ -0,0 +1,14 @@
+package hellotests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloControllerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
